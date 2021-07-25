cb3b6face54be323c07fe3cb60cec75d363a3204
zlp <1131808958@qq.com>
2021-07-25 16:40:19 +0800

    ✨ feat: 使用babel模式打包

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -1,14 +1,14 @@
 export default {
-  esm: 'rollup',
-  cjs: 'rollup',
-  extraBabelPlugins: [
-    [
-      'babel-plugin-import',
-      {
-        libraryName: 'antd',
-        libraryDirectory: 'es',
-        style: true,
-      },
-    ],
-  ],
+  target: 'browser',
+  esm: {
+    type: 'babel',
+    minify: false, // 压缩文件
+    importLibToEs: true,
+  },
+  cjs: 'babel',
+  extractCSS: false,
+  lessInBabelMode: true,
+  autoprefixer: {
+    browsers: ['ie>9', 'Safari >= 6'],
+  }
 };
